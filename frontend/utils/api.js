47d243be387c801d6818232f6dbacc4b8ddeb549
Alexander <english3000.org@gmail.com>
2018-02-27 22:00:07 -0800

    ...

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-const PORT = ':3000';
-
 const HOST = process.env.NODE_ENV === 'production' ?
-  'HEROKU URL' : 'localhost';
+  'https://crdwk.herokuapp.com' : 'http://localhost:3000';
 
-export const signUp = user => axios.post(`http://${HOST}${PORT}/api/users`, {user});
-export const updateUser = user => axios.patch(`http://${HOST}${PORT}/api/users/${user.id}`, {user});
-export const signIn = user => axios.post(`http://${HOST}${PORT}/api/session`, {user});
-export const signOut = () => axios.delete(`http://${HOST}${PORT}/api/session`);
+export const signUp = user => axios.post(`${HOST}/api/users`, {user});
+export const updateUser = user => axios.patch(`${HOST}/api/users/${user.id}`, {user});
+export const signIn = user => axios.post(`${HOST}/api/session`, {user});
+export const signOut = () => axios.delete(`${HOST}/api/session`);
 
 // client-side rendering
-export const visitProfile = id => axios.get(`http://${HOST}${PORT}/users/${id}`);
-export const findUsers = query => axios.get( `http://${HOST}${PORT}/api/users`, {params: {query}} );
+export const visitProfile = id => axios.get(`${HOST}/users/${id}`);
+export const findUsers = query => axios.get( `${HOST}/api/users`, {params: {query}} );
