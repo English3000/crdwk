2b0281855962446436a7b198ea92f78ed2c66604
Alexander <english3000.org@gmail.com>
2018-03-01 10:58:40 -0800

    ...

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const HOST = process.env.NODE_ENV === 'production' ?
   'https://crdwk.herokuapp.com' : 'http://localhost:3000';
 
-export const signUp = user => axios.post(`${HOST}/api/users`, {user});
-export const updateUser = user => axios.patch(`${HOST}/api/users/${user.id}`, {user});
-export const signIn = user => axios.post(`${HOST}/api/session`, {user});
-export const signOut = () => axios.delete(`${HOST}/api/session`);
+export const signUp = user => axios.post(`${HOST}/api/users`, {user}, {crossDomain: true});
+export const updateUser = user => axios.patch(`${HOST}/api/users/${user.id}`, {user}, {crossDomain: true});
+export const signIn = user => axios.post(`${HOST}/api/session`, {user}, {crossDomain: true});
+export const signOut = () => axios.delete(`${HOST}/api/session`, {crossDomain: true});
 
 // client-side rendering
-export const visitProfile = id => axios.get(`${HOST}/users/${id}`);
-export const findUsers = query => axios.get( `${HOST}/api/users`, {params: {query}} );
+export const visitProfile = id => axios.get(`${HOST}/users/${id}`, {crossDomain: true});
+export const findUsers = query => axios.get( `${HOST}/api/users`, {params: {query}}, {crossDomain: true});
